2ddd516e4d292de701ac40c8cb1a9f83c9f0190c
tsaie79 <tsaie79@gmail.com>
2024-10-01 17:19:37 +0000

    revise script

--- run-gui.sh
+++ run-gui.sh
@@ -10,6 +10,4 @@ export EXPID=cool
 
 # 3. Run the jar file
 cd build/libs
-export XQUARTZ_DARK_MODE=0
-export GTK_THEME=Adwaita:light
 java -cp ./dpux-1.0-SNAPSHOT.jar org.jlab.epsci.dpux.desktop.CDesktopNew
